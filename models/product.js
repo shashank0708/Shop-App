32ca1fda404312f990c824f0aae0ef15770b8def
shashank0708 <shashank.mishra2706@gmail.com>
2020-06-19 21:18:47 +0530

    Backbone

---
+++ models/product.js
@@ -0,0 +1,10 @@
+class Product{
+    constructor(id, ownerId, title, imageUrl, description) {
+        this.id = id
+        this.title = title
+        this.ownerId = ownerId
+        this.imageUrl = imageUrl
+        this.description = description
+
+    }
+}
---
+++ screen/shop/ProductsOverviewScreen.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import {FlatList, StyleSheet} from 'react-native'
+
+const ProductsOverviewScreen = props => {
+    return (
+        <FlatList />
+    )
+}
